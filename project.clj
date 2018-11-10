08492a7c8c73c914c7c92420d4e0af46ad3afaf9
Francois <fmjrey>
2018-11-10 16:13:21 +0000

    Upgraded kafka-clients to 2.0.1

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
           :metadata   {:doc/format :markdown}}
   :dependencies [[org.clojure/clojure            "1.9.0"]
                  [org.clojure/core.async         "0.4.474"]
-                 [org.apache.kafka/kafka-clients "2.0.0"]
+                 [org.apache.kafka/kafka-clients "2.0.1"]
                  [cheshire                       "5.8.1"]]
   :test-selectors {:default     (complement :integration)
                    :integration :integration
